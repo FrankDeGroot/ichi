20af9880a149bf4a25c0f620c1b47ad116837ab4
FrankDeGroot <fjtdg@hotmail.com>
2025-02-01 07:36:20 +0000

    start of game test

--- game/game.test.ts
+++ game/game.test.ts
@@ -1,3 +1,20 @@
 import assert from "node:assert";
 import test from "node:test";
 import { Game } from "./game.ts";
+import DiscardPile from "./discard-pile.ts";
+import DrawPile from "./draw-pile.ts";
+
+test("Should play normal game", () => {
+  const discardPile = new DiscardPile({
+    color: "Red", digit: 1
+  })
+  const game = new Game({
+    discardPile,
+    drawPile: new DrawPile([{
+      color: "Red", digit: 2
+    }], discardPile),
+    players: [
+    ]
+  });
+  game.play();
+});
