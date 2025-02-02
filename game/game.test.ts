b079acbc6a81c2ccaadda6636a307d03d31ee02b
FrankDeGroot <fjtdg@hotmail.com>
2025-02-02 11:11:44 +0000

    started on reverse

--- game/game.test.ts
+++ game/game.test.ts
@@ -1,63 +1,87 @@
 import assert from "node:assert";
 import test from "node:test";
-import { Game } from "./game.ts";
+import { Dealing } from "./dealer.ts";
+import { Card } from "./deck.ts";
 import DiscardPile from "./discard-pile.ts";
 import DrawPile from "./draw-pile.ts";
-import Player from "./player.ts";
+import { Game } from "./game.ts";
 import Hand from "./hand.ts";
 import { naiveDiscarder } from "./naive-discarder.ts";
+import Player from "./player.ts";
 
-test("Should play normal game", () => {
-  const discardPile = new DiscardPile({
-    color: "Red",
-    digit: 1,
-  });
-  const drawPile = new DrawPile([{
-    color: "Red",
-    digit: 2,
-  }], discardPile);
-  const players = [
-    new Player(
-      "0",
-      new Hand([{
-        color: "Red",
-        digit: 3,
-      }, {
-        color: "Blue",
-        digit: 9,
-      }]),
-      drawPile,
-      discardPile,
-      naiveDiscarder,
-    ),
-    new Player(
-      "1",
-      new Hand([{
-        color: "Red",
-        digit: 4,
-      }, {
-        color: "Blue",
-        digit: 8,
-      }]),
-      drawPile,
-      discardPile,
-      naiveDiscarder,
-    ),
-    new Player(
-      "2",
-      new Hand([{
-        color: "Red",
-        digit: 5,
-      }]),
-      drawPile,
-      discardPile,
-      naiveDiscarder,
-    ),
-  ];
-  const game = new Game({
+function testDeal({ top, draw, hands }: {
+  top: Card;
+  draw: Card[];
+  hands: Card[][];
+}): Dealing {
+  const discardPile = new DiscardPile(top);
+  const drawPile = new DrawPile(draw, discardPile);
+  const players = Array.apply(null, Array(hands.length))
+    .map((_, i) =>
+      new Player(
+        i.toString(),
+        new Hand(hands[i]),
+        drawPile,
+        discardPile,
+        naiveDiscarder,
+      )
+    );
+  return {
     discardPile,
     drawPile,
     players,
+  };
+}
+
+test("Should play normal game", () => {
+  const { discardPile, drawPile, players } = testDeal({
+    top: {
+      color: "Red",
+      digit: 1,
+    },
+    draw: [{
+      color: "Red",
+      digit: 2,
+    }],
+    hands: [[{
+      color: "Red",
+      digit: 3,
+    }, {
+      color: "Blue",
+      digit: 9,
+    }], [{
+      color: "Red",
+      digit: 4,
+    }, {
+      color: "Blue",
+      digit: 8,
+    }], [{
+      color: "Red",
+      digit: 5,
+    }]],
   });
+  const game = new Game({ discardPile, drawPile, players });
   assert.strictEqual(game.play(), players[2]);
+});
+
+test("A Reverse on top of the discard pile should reverse turn direction", () => {
+  const { discardPile, drawPile, players } = testDeal({
+    top: {
+      color: "Red",
+      specialCard: "Reverse",
+    },
+    draw: [],
+    hands: [[{
+      color: "Red",
+      digit: 1,
+    }], [{
+      color: "Red",
+      digit: 2,
+    }], [{
+      color: "Red",
+      digit: 3,
+    }]],
+  });
+  const game = new Game({ discardPile, drawPile, players });
+  assert.strictEqual(game.play(), players[3]);
 });
